9c6b083fe204cdeac2734bb9a8697d82c99cb6c3
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-10-29 22:47:34 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,82 +3,48 @@ import './App.css';
 
 interface Card {
   id: string;
-  content: string;
+  title: string;
 }
 
-interface List {
+interface Container {
   id: string;
   title: string;
   cards: Card[];
 }
 
 function App() {
-  const [lists, setLists] = useState<List[]>([])
-  const [newCardContent, setNewCardContent] = useState('');
-  const [newListTitle, setNewListTitle] = useState('');
+  const [containers, setContainers] = useState<Container[]>([])
+  const [containerTitle, setContainerTitle] = useState('');
 
-  const addCard = (listId:string) => {
-    if(newCardContent.trim() === ''){return}
-    const updatedLists = lists.map(list => {
-      if(list.id === listId){
-        return{
-          ...list,
-          cards: [...list.cards, {
-            id: Date.now().toString(),
-            content: newCardContent
-          }]
-        };
-      }
-      return list;
-    })
-    setLists(updatedLists);
-    setNewCardContent('');
-  }
+  const addContainer = () => {
+    if(containerTitle.trim() === ""){return}
 
-  const addList = () => {
-    if(newListTitle.trim() === ''){return}
-    const newList:List = {
-      id: Date.now().toString(),
-      title: newListTitle,
-      cards:[]
-    };
-    setLists([...lists, newList]);
-    setNewListTitle('');
-  }
-
-  const deleteList = (listId:string) => {
-    const updatedLists = lists.filter(list => list.id != listId);
-    setLists(updatedLists);
+    setContainers([
+      ...containers,
+      { id: crypto.randomUUID(), title: containerTitle, cards: []},
+    ]);
+    setContainerTitle("");
   }
 
   return (
-    <div className="App">
+  <div className="App">
     <h2>Trello Clone</h2>
-    <div className="list-container">
-    <div className="add-list">
-      <input 
-      type="text"
-      value={newListTitle}
-      onChange={e => setNewListTitle(e.target.value)}
-      placeholder='Add list title'
-      />
-      <button onClick={addList}>Add List</button>
-    </div>
+
+    <input
+      value={containerTitle}
+      onChange={(e) => setContainerTitle(e.target.value)}
+      placeholder='Enter Container Title'
+      style={{ padding: 8, marginRight: 8}}
+    />
    
-      {lists.map(list => (
-        <div
-          key={list.id}
-          className="list"
-          >
-        <h2>{list.title}</h2>
-        <button onClick={() => deleteList(list.id)}>Delete Title</button>
-        <div className="card-content">
-        </div>
-        </div>
-        
-      ))}
+    <button onClick={addContainer}>Add Container</button>
+      {containers.map((col) => (
+    <div className='list-container'>
+          <h2>{col.title}</h2>
     </div>
-     </div>
+      ))}
+
+</div>
   );
 }
 
