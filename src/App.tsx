1dc465c0e5179aff0da8e7fa37817baac1b9d0a7
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-10-29 15:15:05 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface Card {
+  id: string;
+  content: string;
+}
+
+interface List {
+  id: string;
+  title: string;
+  cards: Card[];
+}
+
 function App() {
+  const [lists, setLists] = useState<List[]>([])
+  const [newCardContent, setNewCardContent] = useState('');
+  const [newListTitle, setNewListTitle] = useState('');
+
+  const addCard = (listId:string) => {
+    if(newCardContent.trim() === ''){return}
+    const updatedLists = lists.map(list => {
+      if(list.id === listId){
+        return{
+          ...list,
+          cards: [...list.cards, {
+            id: Date.now().toString(),
+            content: newCardContent
+          }]
+        };
+      }
+      return list;
+    })
+    setLists(updatedLists);
+    setNewCardContent('');
+  }
+
+  const addList = () => {
+    if(newListTitle.trim() === ''){return}
+    const newList:List = {
+      id: Date.now().toString(),
+      title: newListTitle,
+      cards:[]
+    };
+    setLists([...lists, newList]);
+    setNewListTitle('');
+  }
+
+  const deleteList = (listId:string) => {
+    const updatedLists = lists.filter(list => list.id != listId);
+    setLists(updatedLists);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Trello Clone</h1>
+    <div className="add-list">
+      <input 
+      type="text"
+      value={newListTitle}
+      onChange={e => setNewListTitle(e.target.value)}
+      placeholder='Add list title'
+      />
+      <button onClick={addList}>Add List</button>
+    </div>
+    
+    <div className="list-container">
+      {lists.map(list => (
+        <div
+          key={list.id}
+          className="list"
+          >
+        <h2>{list.title}</h2>
+        <button onClick={() => deleteList(list.id)}>Delete Title</button>
+        <div className="card-content">
+        </div>
+        </div>
+        
+      ))}
+    </div>
     </div>
   );
 }
