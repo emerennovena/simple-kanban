d4ae1a37185a2ac0bb47d26d08ddde982cb94398
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-10-30 00:23:06 +0000

    update addContainer and addCard

--- src/App.tsx
+++ src/App.tsx
@@ -15,36 +15,89 @@ interface Container {
 function App() {
   const [containers, setContainers] = useState<Container[]>([])
   const [containerTitle, setContainerTitle] = useState('');
+  const [showInput, setShowInput ] = useState(false);
+  const [cardInputs, setCardInputs] = useState<{ [key: string]: string }>({});
 
-  const addContainer = () => {
-    if(containerTitle.trim() === ""){return}
+  const handleAddClick = () => {
+    const newContainerId = crypto.randomUUID();
+    if(!showInput){
+      setShowInput(true);
+      return;
+    }
 
+    setCardInputs({ ...cardInputs, [newContainerId]: ''});
+    
+    if(containerTitle.trim() === ''){return}
     setContainers([
       ...containers,
-      { id: crypto.randomUUID(), title: containerTitle, cards: []},
+      {id: crypto.randomUUID(), title: containerTitle, cards: [],}
     ]);
-    setContainerTitle("");
-  }
+    
+    setContainerTitle('');
+    setShowInput(false);
+  };
+
+const addCard = (containerId: string) => {
+  const cardContent = cardInputs[containerId];
+  if (!cardContent || cardContent.trim() === '') return;
+
+  setContainers(containers.map((col) => 
+    col.id === containerId
+      ? {
+          ...col,
+          cards: [
+            ...col.cards,
+            { id: crypto.randomUUID(), title: cardContent },
+          ],
+        }
+      : col
+  ));
+
+  setCardInputs({ ...cardInputs, [containerId]: ''});
+};
+
 
   return (
   <div className="App">
-    <h2>Trello Clone</h2>
+    <h2>Kanban</h2>
+    <div className='board'>
+    <button onClick={handleAddClick}>
+      { showInput ? 'Save Container' : 'Add Container'}
+    </button>
 
-    <input
-      value={containerTitle}
-      onChange={(e) => setContainerTitle(e.target.value)}
-      placeholder='Enter Container Title'
-      style={{ padding: 8, marginRight: 8}}
-    />
-   
-    <button onClick={addContainer}>Add Container</button>
-      {containers.map((col) => (
-    <div className='list-container'>
-          <h2>{col.title}</h2>
-    </div>
-      ))}
+    {showInput && (
+      <input
+        value={containerTitle}
+        onChange={(e) => setContainerTitle(e.target.value)}
+        placeholder='Enter Container Title'
+        style={{padding:8, marginTop:8}}
+      />
+    )}
 
-</div>
+    {containers.map((col) => (
+      <div className="list-container" key={col.id}>
+        <h2>{col.title}</h2>
+
+        {col.cards.map((card) => (
+           
+          <div className="list-card" key={card.id}>
+          {card.title}
+          </div>
+    
+        ))}
+        <input
+          value={cardInputs[col.id] || ''}
+          onChange={(e) => setCardInputs({ ...cardInputs, [col.id]: e.target.value})}
+          placeholder='Enter card content'
+        />
+        <button onClick={() => addCard(col.id)}>
+          Add Card
+        </button>
+      </div>
+    ))}   
+
+  </div>
+  </div>
   );
 }
 
